09c09fef6a549378d40593867594a02135201c56
Enrique <Enriquemk51@gmail.com>
2021-10-29 12:55:41 -0400

    Page

---
+++ componentsPhp/header.php
@@ -0,0 +1,34 @@
+<header id="header">
+    <!-- Nav -->
+    <div class="Container-nav-logo">
+      <img src="assets/Icon.png" alt="Logo">
+      <a href="Home.html" class="Logo">GeneaCoach</a>
+    </div>
+    
+    
+    
+    <nav class="nav">
+        <ul class="List">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Curso</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Blog</a>
+          </li>
+          <li class="nav_item">
+            <a class="nav-link" href="#">Enseña con nosotros</a>
+          </li>
+        </ul>
+
+        <div class="Botones">
+          <a id="sign-in-btn" href="#">Inciar seccion</a>
+          <a id="free-btn" href="#">Comienza gratis</a>
+        </div>
+        <!-- <form action="">
+          <button id="sign-in-btn">Inciar seccion</button>
+          <button id="free-btn">Comienza gratis</button>
+        </form> -->
+    </nav>
+
+      <!-- End Nav -->
+    </header>
