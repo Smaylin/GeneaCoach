d7d509781d7110632ce42213572a959dd4ed31d8
Enrique <Enriquemk51@gmail.com>
2021-11-01 15:41:13 -0400

    Nuevo footer

---
+++ componentsPhp/footer.php
@@ -0,0 +1,13 @@
+    <!-- Footer -->
+    <footer>
+      <div>
+        <span>
+          © Todos los derechos reservados geneacoach 2021
+        </span>
+      </div>
+      
+      <div>
+        <a href="#">English</a>
+      </div>
+    </footer>
+    <!-- End-Footer -->
