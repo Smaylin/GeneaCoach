951f2664c04736ccec638f419777e183f56d63c8
Enrique <Enriquemk51@gmail.com>
2021-11-01 15:39:49 -0400

    Conexion de smaylin

---
+++ conexion1.php
@@ -0,0 +1,12 @@
+<?php
+
+// $conexion = mysqli_connect("localhost","root","","rol");
+
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "rol";
+
+$conexion = mysqli_connect($host,$user,$pass,$db);
+?>
